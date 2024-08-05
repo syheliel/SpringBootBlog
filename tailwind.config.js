4201904f1edd823c404c4f34140c070394866cce
syheliel <syheliel@gmail.com>
2024-08-05 17:58:45 +0800

    Fix: upload npm dependency

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "src/main/resources/**/*.html"
+  ],
+  safelist: ["list-inside","text-center"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
